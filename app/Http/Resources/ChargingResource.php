22d7e518eee7b6e023baf2add6b96ded0635c7db
Ryan Joseph P. Dabu <rdabu@reddragonfarm.com>
2025-06-18 16:57:23 +0800

    company to location add id

--- app/Http/Resources/ChargingResource.php
+++ app/Http/Resources/ChargingResource.php
@@ -19,21 +19,27 @@ class ChargingResource extends JsonResource
             "code" => $this->code,
             "name" => $this->name,
 
+            "company_id" => $this->company->id,
             "company_code" => $this->company->code,
             "company_name" => $this->company->name,
 
+            "business_unit_id" => $this->business_unit->id,
             "business_unit_code" => $this->business_unit->code,
-            "business_unit_name" => $this->business_unit->name,
+            "business_unit_name" => $this->business_unit->name, 
 
+            "department_id" => $this->department->id,
             "department_code" => $this->department->code,
             "department_name" => $this->department->name,
 
+            "unit_id" => $this->department_unit->id,
             "unit_code" => $this->department_unit->code,
             "unit_name" => $this->department_unit->name,
 
+            "sub_unit_id" => $this->sub_unit->id,
             "sub_unit_code" => $this->sub_unit->code,
             "sub_unit_name" => $this->sub_unit->name,
 
+            "location_id" => $this->location->id,
             "location_code" => $this->location->code,
             "location_name" => $this->location->name,
 
