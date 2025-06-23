cd618737c866b6c07d8d3aba0eef687de0b57463
Ryan Joseph P. Dabu <rdabu@reddragonfarm.com>
2025-06-23 16:13:14 +0800

    charging search

--- app/Filters/ChargingFilters.php
+++ app/Filters/ChargingFilters.php
@@ -8,5 +8,5 @@ class ChargingFilters extends QueryFilters
 {
     protected array $allowedFilters = [];
 
-    protected array $columnSearch = [];
+    protected array $columnSearch = ["code", "name"];
 }
