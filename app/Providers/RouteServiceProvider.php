a1b35e06ca469835a506edd708b974fcad669151
Ryan Joseph P. Dabu <rdabu@reddragonfarm.com>
2025-08-06 17:20:20 +0800

    august 5

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -41,9 +41,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting(): void
     {
         RateLimiter::for("api", function (Request $request) {
-            return Limit::perMinute(1000)->by(
-                $request->user()?->id ?: $request->ip()
-            );
+            return Limit::none()->by($request->user()?->id ?: $request->ip());
         });
     }
 }
